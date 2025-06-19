9718ccaeabf1b7eb4f4ffc5c6fe600dbe8f71454
kry0z1 <kryoz.n.a@gmail.com>
2025-06-19 13:45:09 +0300

    added workflow

--- hello/main.go
+++ hello/main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.Handle("GET /", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello"))
+		w.Write([]byte("Hello changed"))
 	}))
 	http.ListenAndServe(":8080", nil)
 }

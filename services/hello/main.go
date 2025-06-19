39b1d26250d66509d811930e14bf6561de1460c4
kry0z1 <kryoz.n.a@gmail.com>
2025-06-19 15:21:36 +0300

    another fixing attempt

--- services/hello/main.go
+++ services/hello/main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.Handle("GET /", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello changed again and again and again"))
+		w.Write([]byte("Hello changed again and again and again and again"))
 	}))
 	http.ListenAndServe(":8080", nil)
 }
